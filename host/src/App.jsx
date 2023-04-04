8b2696d2539768a51a3009409c98bd23d1fba249
Luis Merino <luis.al.merino@gmail.com>
2023-04-04 15:24:52 +0200

    share remote Counter to host by module federation

--- host/src/App.jsx
+++ host/src/App.jsx
@@ -1,13 +1,13 @@
-import { render } from "solid-js/web";
+import { render } from 'solid-js/web';
 
-import "./index.scss";
+import Counter from 'remote/Counter';
+
+import './index.scss';
 
 const App = () => (
-  <div class="mt-10 text-3xl mx-auto max-w-6xl">
+  <div class='mt-10 text-3xl mx-auto max-w-6xl'>
     <div>Name: host</div>
-    <div>Framework: solid-js</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
+    <Counter />
   </div>
 );
-render(App, document.getElementById("app"));
+render(App, document.getElementById('app'));
